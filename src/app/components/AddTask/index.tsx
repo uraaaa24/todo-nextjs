8c80e69e0dafb102e9fd507f92329cc88abcd862
Yuto Urashima <yuto.urashima24@gmail.com>
2023-10-19 12:03:24 +0900

    Create AddTask component

--- src/app/components/AddTask/index.tsx
+++ src/app/components/AddTask/index.tsx
@@ -1,7 +1,10 @@
 
 const AddTask = () => {
   return (
-    <div>AddTask</div>
+    <form className="mb-4 space-y-3">
+      <input type="text" className="w-full border px-4 rounded-lg focus:outline-none focus:border-blue-300"/>
+      <button className="w-full px-4 py-2 text-white bg-blue-500 rounded transform hover:bg-blue-300 hover:scale-95 duration-200">Add Task</button>
+    </form>
   )
 }
 
